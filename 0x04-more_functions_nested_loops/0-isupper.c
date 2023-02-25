cab376826f0f77006f7d8bd966153787f0ff7ad5
Blackjey05 <onimbojerry@gmail.com>
2023-02-24 16:14:30 -0800

    Task 0 - 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c: characters to be checked
+ * Return: 1 if upperrcase,0 if otherwise
+ */
+int _isupper( int c)
+{
+	if (c>= 'A' && c<='Z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
