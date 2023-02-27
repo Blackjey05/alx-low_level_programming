ab4bd1033c5d5474998b7d1150e25c21fa2a684a
Blackjey05 <onimbojerry@gmail.com>
2023-02-27 10:08:24 -0800

    Correction on task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,15 +4,13 @@
 /**
  * swap_int - swaps the values of two integers
  * @a: first integer
- * (*b): second integer
+ * @b: second integer
  *
  * Return: nothing
  */
 void swap_int(int *a, int *b)
-{
-		
+{		
 	*a ^= *b;
 	*b ^= *a;
-	*a ^= *b;
-	
+	*a ^= *b;	
 }
