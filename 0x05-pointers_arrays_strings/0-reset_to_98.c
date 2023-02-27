51916840a63f6baa7cf8e344457a35b9cecd8b3a
Blackjey05 <onimbojerry@gmail.com>
2023-02-27 08:54:04 -0800

    Updating command for 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,19 +11,3 @@ void reset_to_98(int *n)
 	*n=98;
 }
 
-/**
- * main -  updates the value it points to to 98
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int p;
-	int *n;
-
-	n=&p;
-	p=420;
-	reset_to_98(n);
-	printf("Value of 'p' after the call:p=%d\n",p);
-	return(0);
-}
