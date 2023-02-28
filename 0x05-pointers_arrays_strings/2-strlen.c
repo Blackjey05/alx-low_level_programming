a58b30a4ef23d40f67907e59bf5d4acca467dc15
Blackjey05 <onimbojerry@gmail.com>
2023-02-27 17:57:03 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include <stdio.h>
+#include<string.h>
+
+/**
+ * strlen -calculates the length of a string
+ *
+ * Return: Always 0
+ */
+int _strlen(char *str)
+{
+  int length = 0;
+  while (*str != '\0') {
+    length++;
+    str++;
+  }
+  return length;
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * puts- prints a string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _puts(char *str)
+{
+	_puts(str);	
+}
