78bdd440d462dc456dcf7297c54de668fcd9f010
Blackjey05 <onimbojerry@gmail.com>
2023-03-01 02:50:55 -0800

    Betty correction for task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,17 @@
 #include<stdio.h>
 
 /**
- * puts- prints a string
+ *_puts- prints a string
+ *@str: place holder for the string to be executed
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void _puts(char *str)
 {
-	while(*str)
+	while (*str)
 	{
 		putchar(*str);
 		str++;
-	}
-		
+	}	
 }
