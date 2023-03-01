8ca9167447d3cb03de08c09c50daaefa33790422
Blackjey05 <onimbojerry@gmail.com>
2023-02-28 17:43:05 -0800

    Betty style correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,10 @@ void rev_string(char *s)
 	int length = strlen(s);
 	int i;
 
-	for(i = 0; i < length/2; i++)
+	for (i = 0; i < length /2; i++)
 	{
 		char temp = s[i];
-		s[i] = s[length-i-1];
-		s[length-i-1] = temp;
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
 	}
 }
