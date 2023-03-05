31d5b1beadd97fd9a21e46a1638d4b1f1e6307bf
Blackjey05 <onimbojerry@gmail.com>
2023-03-05 00:53:48 -0800

    Correction for task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mult = 1; mult <= 9;mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -26,5 +26,5 @@ void times_table(void)
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
-	}		
+	}
 }
