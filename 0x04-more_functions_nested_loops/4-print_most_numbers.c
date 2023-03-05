8387c553194bdde726640f65aa66b356049aa9f9
Blackjey05 <onimbojerry@gmail.com>
2023-03-05 05:03:56 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,18 @@
  */
 void print_most_numbers(void)
 {
-	int c;
+	int c = 0;
 
-	for(c = 48; c<= 57; c++)
+	for (; c <= 9; c++)
 	{
-		if(c != 50 ||c != 52)
-			{
-			_putchar(c);
-			}
+		if (c == 2 || c == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c + '0');
+		}
 	}
 	_putchar('\n');
 }
