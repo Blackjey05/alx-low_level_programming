d692b9ee02a26b1ea419cf2dbc0e47dfd2e30a4b
Blackjey05 <onimbojerry@gmail.com>
2023-03-05 06:08:09 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * main - check the code
+ *print_diagonal - prints characters diagonally
+ *
+ *Return: Always (0)
+ */
+int main(void)
+{
+	print_diagonal(0);
+		print_diagonal(2);
+			print_diagonal(10);
+				print_diagonal(-4);
+				return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,6 @@
 
 void print_triangle(int size);
 void print_square(int size);
-void print_diagonal(int n);
 void print_line(int n);
 void more_numbers(void);
 void print_most_numbers(void);
@@ -12,5 +11,6 @@ int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
 int _putchar(char c);
+void print_diagonal(int n);
 
 #endif
