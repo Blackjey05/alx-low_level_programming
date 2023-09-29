111a76699b027dd8c0d18a923347bcafbdb7f0a2
Blackjey05 <onimbojerry@gmail.com>
2023-09-29 05:26:25 -0700

    Betty style correction for task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include"main.h"
 
-/*_strlen_recursion- a function that prints the length of a string
+/*strlen_recursion- a function that prints the length of a string
  *@s:placeholder for string
  *
  *Return:1 on success and 0 when string is done.
@@ -9,6 +9,6 @@
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
-	return 1 + _strlen_recursion(s + 1);
+		return(0);
+	return(1) + _strlen_recursion(s + 1);
 }
