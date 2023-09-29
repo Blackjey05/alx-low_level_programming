5ac79487d4568c1c7ee1accbf223bd5830cf50f6
Blackjey05 <onimbojerry@gmail.com>
2023-09-29 01:10:17 -0700

    Task 0:created the necessary files needed i.e attempt 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *_puts_recursion- a function that prints a string
+ *@s: character string
+ *_putchar - a function that prints character
+ */
+void _puts_recursion(char *s)
+{
+	if (s == NULL)
+		/*return;*/
+		_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	/*_putchar('\n');*/
+}
