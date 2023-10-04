5cf9bbbbfeb3162225ec8071bf820f96cab750de
Blackjey05 <onimbojerry@gmail.com>
2023-10-04 06:01:10 -0700

    Attempt 0 on task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+
+/*pow_recursion - function that returns the power of a number
+ *@x: integer
+ *@y: place value of ower
+ *
+ * Return : (0) on success and 1 on success
+ */
+int _pow_recursion(int x, int y)
+{
+	if(y < 0)
+		return -1;
+	if(y <= 1)
+		return 1;
+	return(x * x) + _pow_recursion(x, y -1);
+}
